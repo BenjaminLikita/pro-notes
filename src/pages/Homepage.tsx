f1b3822f6fdfd3d132f8fb42f63961201f089d32
Benjamin Likita <benjaminlikita3@gmail.com>
2025-02-12 19:39:54 +0100

    made changes

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -14,13 +14,13 @@ const Homepage = () => {
             <h1 className="text-3xl font-bold">Write to your Hearts Content...</h1>
         </div>
         <hr />
-        <div className="grid grid-cols-3 gap-10 items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 items-center">
             <Link to={"/create"} className='bg-[#35414f] grid place-items-center rounded-lg shadow-lg hover:opacity-80 h-[250px] transition-all duration-300'>
                 <h2 className="flex items-center gap-3 text-xl font-semibold"><Plus /> Add Notes</h2>
             </Link>
             {
                 !notes?.length ? (
-                    <h1 className="font-bold text-xl">No Notes Available...</h1>
+                    <h1 className="font-bold text-xl text-center">No Notes Available...</h1>
                 ): (
                     notes.map(note => (
                         <Card key={note.id} {...note} />
